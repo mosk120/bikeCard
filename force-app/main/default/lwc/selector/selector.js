1d7f8adaf6206e802d6b435bfe621fc3096618e2
mosk120-username <mosk1488@gmail.com>
2021-02-24 11:51:27 +0100

    commit2

--- force-app/main/default/lwc/selector/selector.js
+++ force-app/main/default/lwc/selector/selector.js
@@ -1,8 +1,10 @@
-import { LightningElement } from 'lwc';
-
+import { LightningElement, wire } from 'lwc';
+import { getRecord, getFieldValue } from 'lightning/uiRecordApi';
+import Id from '@salesforce/user/Id';
+import NAME_FIELD from '@salesforce/schema/User.Name';
+const fields = [NAME_FIELD];
 export default class Selector extends LightningElement {
     selectedProductId;
-
     handleProductSelected(evt) {
         this.selectedProductId = evt.detail;
     }
@@ -11,4 +13,5 @@ export default class Selector extends LightningElement {
     user;
     get name() {
         return getFieldValue(this.user.data, NAME_FIELD);
+    }
 }
